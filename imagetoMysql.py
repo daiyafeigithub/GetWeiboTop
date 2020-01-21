5c84e2fa0d09aaf174d46695a7ea769e2dfb1a04
daiyafeigithub <daiyafeihotmail@qq.com>
2020-01-21 13:52:37 +0800

    2020年1月21日13:51:40 按自己数据库格式修改了插入的python文件 使用uuid 增加了conn.commit()等

--- imagetoMysql.py
+++ imagetoMysql.py
@@ -2,30 +2,47 @@ import time
 import pymysql
 import base64
 import os
+import uuid as uid
 
 mysql_conn = pymysql.connect('localhost', 'root', 'root', 'asusee')
 cursor = mysql_conn.cursor()
-def  transferToBase64(imaurl):
+
+
+def transferToBase64(imaurl):
     with open(imaurl, 'rb') as f:
         base64_data = base64.b64encode(f.read())
-        s=base64_data.decode()
+        s = base64_data.decode()
         return s
+
+
 def connectmysql(valuelist):
-    paramlist="base64,size,imagename,createtime,updatetime,userip,sourceflag,imagetypeid,userid,status"
-    insertsql='insert into t_bizhi(' + paramlist + ') values('+ valuelist +')'
-    #cursor.execute(insertsql)
+    paramlist = "imagecontent,size,imagename,createtime,updatetime,userip,sourceflag,imagetypeid,userid,status,imagetitle"
+    insertsql = 'insert into t_bizhi(' + paramlist + ') values(' + valuelist + ')'
+    print("开始执行--->"+insertsql)
+    cursor.execute(insertsql)
+    print("结束")
+
 if __name__ == "__main__":
 
+    pic_dir = "D:\\gallery"  # default dir
+    str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+    list = os.listdir(pic_dir)  # 列出文件夹下所有的目录与文件
+    namelist = []
+    with open('D://bingpicnames.txt', 'r', encoding="utf-8") as f:
+        namelist = f.readlines()
+    for i in range(len(list)):
 
-  pic_dir = "D:\\images\\Pictures\\Bing"  # default dir
-  str_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-  list = os.listdir(pic_dir)  # 列出文件夹下所有的目录与文件
-  for file in list:
-      imgname = os.path.join(pic_dir, file)
-      b64=transferToBase64(imgname);
-      print(imgname)
+        imgname = os.path.join(pic_dir, str(i + 1) + ".jpg")
+        b64 = transferToBase64(imgname);
+        imgname = str(uid.uuid1()).replace("-", "") + "_" + str(i + 1) + ".jpg"
+        if namelist[i].count("'") > 0:
+            str1 = namelist[i].replace("'", "\\'")
+            print(str1)
+            valuelist = "'" + b64 + "','1980x1080','" + imgname + "','" + str_time + "','" + str_time + "','localhost','1','21','1','1','" + str1 + "'"
+        else:
+            valuelist = "'" + b64 + "','1980x1080','" + imgname + "','" + str_time + "','" + str_time + "','localhost','1','21','1','1','" + str(namelist[i]) + "'"
+        connectmysql(valuelist)
+    cursor.close()
+    mysql_conn.commit();
+    mysql_conn.close()
 
-      valuelist="'"+b64+"','1980x1080','"+imgname+"','"+str_time+"','"+str_time+"','localhost','1','21','1','1'"
-      connectmysql(valuelist)
-  mysql_conn.close()
-  cursor.close()
