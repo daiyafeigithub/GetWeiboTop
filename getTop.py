532ef1ad7a0245b6408eb78ede4025f88e031119
daiyafeigithub <daiyafeihotmail@qq.com>
2020-02-01 15:31:06 +0800

    2020年2月1日15:30:49 修正了实时百度热搜

--- getTop.py
+++ getTop.py
@@ -31,7 +31,7 @@ def getweibo():
 #baidu
 
 head = {}
-url = "http://top.baidu.com/buzz?b=341&fr=topindex"
+url = "http://top.baidu.com/buzz?b=1&c=513&fr=topbuzz_b341_c513"
 head["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:63.0) Gecko/20100101 Firefox/63.0"
 head["Accept"]= "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8"
 head["Accept-Language"]= "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2"
